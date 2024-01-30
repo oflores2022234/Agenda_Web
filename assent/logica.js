eb6f75fcb9fdcf6e2ec7c72ebda1578d3c2976b6
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-01-30 15:52:45 -0600

    agregamos boton de ver más en contacto, junto con la opcion de ocultar la etiqueta, y realizamos la logica de mostrar y ocultar información en js

---
+++ assent/logica.js
@@ -0,0 +1,10 @@
+function mostrarOcultar(){
+
+    var etiqueta = document.getElementById("etiqueta");
+    if (etiqueta.style.display === "none") {
+        etiqueta.style.display = "block";
+      } else {
+        etiqueta.style.display = "none";
+      }
+
+}
